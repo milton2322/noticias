94db7215fcb4d4d2edae8f2e89120eb958021c32
milton2223 <milton.quinones@iteria.com.co>
2020-12-20 17:37:25 -0500

    changes in services file

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -22,7 +22,7 @@ export class NoticiasService {
   private ejecutarQuery<T>(query: string){
 
     query = apiUrl + query;
-    console.log(query);
+    console.log("pruebas",query);
     
     return this.http.get<T>( query, {headers}); 
 
